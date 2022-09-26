3e0ede2957e472c56d9ed037265224e57556270b
Michel Koyang-Kuleshov <michel@koyang-kuleshov.ru>
2022-09-26 23:33:18 +0300

    Added test

--- mock-service/django/mockservice/mainapp/tests.py
+++ mock-service/django/mockservice/mainapp/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.urls import reverse
+
+
+class TestMainappSmoke:
+
+    @pytest.mark.parametrize('name ,code', [
+        ('mainapp:get-status', 200),
+        ('mainapp:post-message', 200),
+            ])
+    def mainapp_test(self, client, name, code):
+        assert client.get(reverse(name)).status_code == code
--- mock-service/django/mockservice/mainapp/urls.py
+++ mock-service/django/mockservice/mainapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import PostMessage, GetStatus
 
 urlpatterns = [
-    path("post/message", PostMessage.as_view()),
-    path("get/status", GetStatus.as_view()),
+    path("post/message", PostMessage.as_view(), name="post-message"),
+    path("get/status", GetStatus.as_view(), name="get-status"),
     ]
 
