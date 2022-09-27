20da09ec20dcfc5e468b3ed154ef734d61013650
Michel Koyang-Kuleshov <michel@koyang-kuleshov.ru>
2022-09-28 00:53:43 +0300

    Added a variable to simulate unstable behavior of the service

--- mock-service/django/mockservice/mainapp/views.py
+++ mock-service/django/mockservice/mainapp/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+from random import randint
+from django.conf import settings
+from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -12,9 +14,14 @@ class PostMessage(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         self.create(request, *args, **kwargs)
-        return Response({"message": "Message added in queue",
-                         "status": 202
-                         })
+        percent = settings.PERCENT_OF_GOOD_RESPONSE
+        if randint(1, 100) < percent:
+            return Response({"message": "Message added in queue",
+                             "status": 202
+                             })
+        return Response("Service unavailable",
+                        status=status.HTTP_503_SERVICE_UNAVAILABLE
+                        )
 
 
 @api_view()
